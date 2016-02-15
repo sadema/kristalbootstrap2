ef314d92d7bf32436b3c7ff12660790f680e13a7
sjoerdadema <sjoerd@kristalsoftware.nl>
2016-02-15 21:19:56 +0100

    refactoring and added eventemitter

--- kristalcmsclient/src/main/webapp/app/navigationbar.component.ts
+++ kristalcmsclient/src/main/webapp/app/navigationbar.component.ts
@@ -8,9 +8,13 @@ import { NavigationBarItem } from './navigationbar_item.component'
     template: `
             <a class="navbar-brand" href="#">{{title}}</a>
             <ul class="nav navbar-nav">
-                <navigationbar-item
-                    *ngFor="#item of itemList">
-                </navigationbar-item>
+                <li
+                    *ngFor = '#item of itemList'
+                    [item] = 'item'
+                    [class.active] = 'isActiveItem(item)'
+                    (selectItemEvent) = 'onItemSelected($event)'
+                    >
+                </li>
             </ul>
     `,
     inputs: ['itemList'],
@@ -18,11 +22,23 @@ import { NavigationBarItem } from './navigationbar_item.component'
 })
 export class NavigationBar {
     title: string;
-    activeItem: number;
+    activeItem: NavigationItem;
     itemList: NavigationItem[];
 
     constructor() {
         this.title = 'prima';
-        this.activeItem = 0;
+        this.activeItem = null;
     }
+
+    isActiveItem(item: NavigationItem): boolean {
+        return item === this.activeItem;
+    }
+
+    onItemSelected(item: NavigationItem): void {
+        console.log("method onItemSelected");
+        if (!item.disabled) {
+            this.activeItem = item;
+        }
+    }
+
 }
--- kristalcmsclient/src/main/webapp/app/navigationbar_item.component.ts
+++ kristalcmsclient/src/main/webapp/app/navigationbar_item.component.ts
@@ -1,14 +1,36 @@
-import { Component } from "angular2/core";
+import { Component } from 'angular2/core';
+import { EventEmitter } from 'angular2/core';
+import {NavigationItem} from './navigation_item';
 
 @Component({
-    selector: 'navigationbar-item',
+    selector: 'li',
+    host: {'class': 'nav-item'},
     template: `
-        <li class='nav-item'>
-            <a class="nav-link"
-                href="#">hello</a>
-        </li>
-    `
+        <a class="nav-link"
+            href="#"
+            [class.disabled] = 'isDisabled(item)'
+            (click) = 'setActiveItem(item)'
+            >
+            {{item.title}}
+        </a>
+    `,
+    inputs: ['item'],
+    outputs: ['selectItemEvent']
 })
 export class NavigationBarItem {
+    item: NavigationItem;
+    selectItemEvent: EventEmitter<NavigationItem>;
+
+    constructor() {
+        this.selectItemEvent = new EventEmitter();
+    }
+
+    isDisabled(item: NavigationItem): boolean {
+        return item.disabled;
+    }
+
+    setActiveItem(item: NavigationItem): void {
+        this.selectItemEvent.emit(item);
+    }
 
 }
