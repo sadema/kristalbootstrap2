7219f6a0f50c94ae8b02cbde00b01b8c947e6449
sjoerdadema <sjoerd@kristalsoftware.nl>
2016-02-15 22:00:25 +0100

    added @RouteConfig

--- kristalcmsclient/src/main/webapp/app/kristalcms.component.ts
+++ kristalcmsclient/src/main/webapp/app/kristalcms.component.ts
@@ -1,16 +1,27 @@
 import { Component } from "angular2/core";
 import {NavigationBar} from "./navigationbar.component";
 import {NavigationItem} from "./navigation_item";
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+import { TemplatesComponent } from './templates.component';
 
+@RouteConfig([
+    {
+        path: 'kristalcmsclient/templates',
+        name: 'Templates',
+        component: TemplatesComponent
+    }
+])
 @Component({
     selector: 'kristalcms-app',
     template: `
         <nav
             [itemList] = "items">
         </nav>
-        <h1>Hello World</h1>
+        <a [routerLink] = "['Templates']">Templates</a>
+        <router-outlet></router-outlet>
     `,
-    directives: [NavigationBar]
+    directives: [NavigationBar, ROUTER_DIRECTIVES],
+    providers: [ROUTER_PROVIDERS]
 })
 export class KristalcmsComponent {
     items: NavigationItem[];
---
+++ kristalcmsclient/src/main/webapp/app/templates.component.ts
@@ -0,0 +1,11 @@
+import { Component } from 'angular2/core';
+
+@Component({
+    selector: 'templates',
+    template: `
+        <h1>Templates</h1>
+    `
+})
+export class TemplatesComponent {
+
+}
